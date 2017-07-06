d5c10e62a4e625e9dd75651b1a8388ca9d77e991
Lee Aylward <lee@laylward.com>
2017-07-06 02:06:51 -0400

    new private implementation

--- lib/API/Controller/Message.pm
+++ lib/API/Controller/Message.pm
@@ -4,7 +4,6 @@ use parent 'API::Controller';
 
 API->register("message.log",      __PACKAGE__);
 API->register("message.missed",   __PACKAGE__);
-API->register("message.privates", __PACKAGE__);
 API->register("message.seen",     __PACKAGE__);
 
 sub missed {
@@ -93,28 +92,6 @@ sub log {
 
   $sth->finish;
   return $app->json(\@data);
-}
-
-sub privates {
-  my ($app, $req) = @_;
-  my $user = $req->session->{user};
-
-  my $sth = $app->dbh->prepare_cached(q{
-    SELECT channel AS nick, log.connection
-    FROM log
-    JOIN connection
-      ON log.connection=connection.id
-    WHERE log.privmsg = TRUE
-    AND connection.user=?
-    AND log.time > LEAST(?, NOW() - INTERVAL '2 WEEK')
-    GROUP BY channel, log.connection
-  });
-
-  $sth->execute($user, $app->last_login);
-  my $rows = $sth->fetchall_arrayref({});
-  $sth->finish;
-
-  $app->json([ grep { $_->{nick} =~ /^[^#&+!]/ } @$rows ]);
 }
 
 sub seen {
---
+++ lib/API/Controller/Private.pm
@@ -0,0 +1,44 @@
+package API::Controller::Message;
+
+use parent 'API::Controller';
+
+API->register("private.list", __PACKAGE__);
+API->register("private.delete", __PACKAGE__);
+API->register("private.create", __PACKAGE__);
+
+sub list {
+  my ($app, $req) = @_;
+  my $user = $req->session->{user};
+
+  my $sth = $app->dbh->prepare_cached(q{
+    SELECT p.nick, p.connection, p.time
+    FROM private AS p
+    JOIN connection AS c
+      ON c.id=p.connection
+    WHERE c.user=?
+  });
+
+  $sth->execute($user);
+  my $rows = $sth->fetchall_arrayref({});
+  $sth->finish;
+
+  $app->json($rows);
+}
+
+sub delete {
+  my ($app, $req) = @_;
+  my $user = $req->session->{user};
+  my $conn = $req->captures->{id};
+  my $nick = $req->captures->{nick};
+
+  my $sth = $app->dbh->prepare_cached(q{
+    DELETE FROM private AS p
+    WHERE p.connection=?
+      AND p.nick=?
+  });
+  $sth->execute($conn, $nick);
+
+  $app->ok;
+}
+
+1;
